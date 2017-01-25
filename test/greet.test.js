e9dd8260daaffab368782015efe397bc0d81519c
cmd-kvn <kvn.m.wong@gmail.com>
2017-01-25 11:33:48 -0800

    passed test for --plain flag

--- test/greet.test.js
+++ test/greet.test.js
@@ -12,7 +12,7 @@ describe('greeting app', function() {
     //     assert.equal(output, 'hello stranger\n');
     // });
 
-    it('greets by name using cowsay', function(){
+    it('uses "stranger" as a default when no name provided using cowsay', function(){
         var output = run(['./lib/greet.js']);
         assert.equal(output, ` ________________
 < hello stranger >
@@ -22,5 +22,10 @@ describe('greeting app', function() {
             (__)\\       )\\/\\
                 ||----w |
                 ||     ||\n`);
+    });
+
+    it('greets without cowsay when a "--plain" flag is added', function(){
+        var output = run(['./lib/greet.js', '--plain']);
+        assert.equal(output, 'hello stranger\n');
     });
 });
