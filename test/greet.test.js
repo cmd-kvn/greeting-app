f72102254640ff88d6fb50af22a4c23aa4677f1d
cmd-kvn <kvn.m.wong@gmail.com>
2017-01-25 11:07:18 -0800

    passed cowsay stranger added pretest to package.json

--- test/greet.test.js
+++ test/greet.test.js
@@ -2,13 +2,25 @@ var assert = require('assert');
 var run = require('./run.js');
 
 describe('greeting app', function() {
-    it('greets by name', function() {
-        var output = run(['./lib/greet.js', 'marty']);
-        assert.equal(output, 'hello marty\n');
-    });
+    // it('greets by name', function() {
+    //     var output = run(['./lib/greet.js', 'marty']);
+    //     assert.equal(output, 'hello marty\n');
+    // });
 
-    it('uses "stranger" as a default when no name provided', function() {
+    // it('uses "stranger" as a default when no name provided', function() {
+    //     var output = run(['./lib/greet.js']);
+    //     assert.equal(output, 'hello stranger\n');
+    // });
+
+    it('greets by name using cowsay', function(){
         var output = run(['./lib/greet.js']);
-        assert.equal(output, 'hello stranger\n');
+        assert.equal(output, ` ________________
+< hello stranger >
+ ----------------
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||\n`);
     });
 });
