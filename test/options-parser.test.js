7dc799c2b142b24fead9e265a182dcdf8cc58a56
cmd-kvn <kvn.m.wong@gmail.com>
2017-04-06 17:35:14 -0700

    finishes unit tests for optionsParser

--- test/options-parser.test.js
+++ test/options-parser.test.js
@@ -5,7 +5,26 @@ describe.only('optionsParser', function () {
     it('correctly handles no arguments', function () {
         let output = optionsParser([]);
         assert.deepEqual(output, {});
-    })
+    });
 
+    it('correctly handles a --plain argument', function () {
+        let output = optionsParser(['--plain']);
+        assert.deepEqual(output, {'plain': true});
+    });
+
+    it('correctly handles a --key:value argument', function () {
+        let output = optionsParser(['--foo:bar']);
+        assert.deepEqual(output, {'foo': 'bar'});
+    });
+
+    it('correctly handles --key:value --key2:value2', function () {
+        let output = optionsParser(['--foo:bar', '--baz:abc']);
+        assert.deepEqual(output, {'foo': 'bar', 'baz': 'abc'});
+    });
+
+    it('correctly handles --plain --key:value', function () {
+        let output = optionsParser(['--plain', '--foo:bar']);
+        assert.deepEqual(output, {'plain': true, 'foo': 'bar'});
+    });
 
 });
